4d83283e0006992c4138152a392dfdc6940bff2b
Arcenio Souza <arcenio_neto@icloud.com>
2022-03-18 17:00:01 -0300

    Corrige bug de validações e padroniza formatado das respostas da api

--- src/controller/FuncionariosController.js
+++ src/controller/FuncionariosController.js
@@ -8,8 +8,8 @@ class FuncionariosController{
    //Método Create --------------------
    createTable(req, res){
       metodos.postTable()
-      .then(response => res.send(response))
-      .catch(response => res.send(response))
+      .then(response => res.sendStatus(201).send(response))
+      .catch(response => res.sendStatus(400).send(response))
    }
 
    //Método Create --------------------
@@ -39,52 +39,71 @@ class FuncionariosController{
             throw new Error("CPF inválido")
          } else {
             metodos.postFuncionario(...dataFuncionario)
-            .then(response => res.send(response))
-            .catch(response => res.send(response))
+            .then(response => res.status(200).send(response))
+            .catch(response => res.status(400).send(response))
          }         
 
       } catch(e){
-         res.send(e.message)
+         res.status(400).send(e.message)
       }
    }
 
    //Método Read ----------------------
    getFuncionarios(req, res){
       metodos.getAllFuncionarios()
-      .then(response => res.send(response))
-      .catch(response => res.send(response))
+      .then(response => res.status(200).send(response))
+      .catch(response => res.status(400).send(response))
    }
 
    //Método Read --------------------
    getFuncionarioId(req, res){
       const id = req.params.id
       metodos.getFuncionarioId(id)
-      .then(response => res.send(response))
-      .catch(response => res.send(response))  
+      .then(response => res.status(200).send(response))
+      .catch(response => res.status(400).send(response))
    }
 
    //Método Delete --------------------
    deleteFuncionario(req, res){
       const id = req.params.id
       metodos.deleteFuncionario(id)
-      .then(response => res.send(response))
-      .catch(response => res.send(response)) 
+      .then(response => res.status(200).send(response))
+      .catch(response => res.status(400).send(response))
    }
 
    //Método Update --------------------
    async updateFuncionario(req, res){
-      const novoFuncionario = await new Promise((resolve, reject) => {         
-         resolve([
-            req.params.id,
-            req.body.nome,
-            req.body.cargo,
-            parseFloat(req.body.salario),
-            parseInt(req.body.cpf)
-         ])
-      })
-      metodos.updateFuncionario(...novoFuncionario)
-      .then(response => res.send(response))
-      .catch(response => res.send(response))       
+      try{
+         const novoFuncionario = await new Promise((resolve, reject) => {         
+            resolve([
+               req.params.id,
+               req.body.nome,
+               req.body.cargo,
+               parseFloat(req.body.salario),
+               parseInt(req.body.cpf)
+            ])
+         })
+
+         //validações
+         const validaNome = validacoes.validaNome(novoFuncionario[1])
+         const validaCargo = validacoes.validaCargo(novoFuncionario[2])
+         const validaCpf = validacoes.validaCPF(String(novoFuncionario[4]))
+
+         if(!validaNome){
+            throw new Error("Problemas com a validação do nome")
+         } else if(!validaCargo){
+            throw new Error("Cargo inexistente")
+         } else if(!validaCpf){
+            throw new Error("CPF inválido")
+         } else {
+            metodos.updateFuncionario(...novoFuncionario)
+            .then(response => res.status(200).send(response))
+            .catch(response => res.status(400).send(response)) 
+         } 
+         
+      } catch(e){
+         res.status(400).send(e.message)
+      }      
    }
 }
 
