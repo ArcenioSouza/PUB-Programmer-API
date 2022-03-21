ccf94567ff59e933c0e10284e89dddbc5d225ff6
Arcenio Souza <arcenio_neto@icloud.com>
2022-03-21 20:10:45 -0300

    cria configuração para dotenv

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ import EmployeesController from './controller/EmployeesController.js';
 import routes from './routes/index.js';
 import dotenv from '.dotenv'
 
+dotenv.config({
+   path: `.env.${process.env.NODE_ENV}`,
+ });
+
 const app = express();
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
