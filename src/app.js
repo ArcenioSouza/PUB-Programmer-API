5a055c9295b2f039660ed70ab796e8799cacba14
Arcenio Souza <arcenio_neto@icloud.com>
2022-03-21 20:13:02 -0300

    Muda app.js para deploy no heroku

--- src/app.js
+++ src/app.js
@@ -1,11 +1,6 @@
 import express from 'express';
 import EmployeesController from './controller/EmployeesController.js';
 import routes from './routes/index.js';
-import dotenv from '.dotenv'
-
-dotenv.config({
-   path: `.env.${process.env.NODE_ENV}`,
- });
 
 const app = express();
 app.use(express.urlencoded({extended: true}))
