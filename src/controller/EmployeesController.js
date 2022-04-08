8dea360860be1e64089f0491a16118ec1907c81a
Arcenio Souza <arcenio_neto@icloud.com>
2022-04-08 16:10:01 -0300

    feat(employeescontroller.js): :sparkles:Add methods get, post, delete, update

--- src/controller/EmployeesController.js
+++ src/controller/EmployeesController.js
@@ -33,53 +33,57 @@ class EmployeesController {
 
    //Método Read ----------------------
    getEmployees(req, res) {
-      //...
+      employees.find((error, employee) => {
+         if(!error)
+         res.status(200).json(employee)
+      })
    }
 
    //Método Read --------------------
    getEmployeeId(req, res) {
       const id = req.params.id;
-      //...
+      employees.findById(id, (error, employee) => {
+         if(!error)
+         res.status(200).json(employee)
+      })
    }
 
    //Método Delete --------------------
    deleteEmployee(req, res) {
       const id = req.params.id;
-      //...
+      employees.findByIdAndDelete(id, (error) => {
+         if(error){
+            res.status(500).send({message: `Fail to delete - ${error}`})
+         } else {
+            res.status(201).send({message: `delete done successfully`})
+         }
+      })
    }
 
    //Método Update --------------------
    async updateEmployee(req, res) {
       try {
-         const Employees = await new Promise((resolve, reject) => {
-            resolve([
-               req.params.id,
-               req.body.name,
-               req.body.office,
-               parseFloat(req.body.wage),
-               parseInt(req.body.cpf),
-            ]);
-
-            reject({
-               "message error": "Problems Accessing Employee Information",
-            });
-         });
-
+         let newEmployee = new employees(req.body)
+         let id = req.params.id
          //validações
-         const validationsName = validations.validationsName(Employees[1]);
-         const validationsOffice = validations.validationsOffice(Employees[2]);
-         const validationsCPF = validations.validationsCPF(
-            String(Employees[4])
-         );
+         const validationsName = validations.validationsName(newEmployee.name);
+         const validationsJob = validations.validationsJob(newEmployee.job);
+         const validationsCPF = validations.validationsCPF(newEmployee.cpf);
 
          if (!validationsName) {
             throw new Error("Problems validating name");
-         } else if (!validationsOffice) {
+         } else if (!validationsJob) {
             throw new Error("Position non-existent");
          } else if (!validationsCPF) {
             throw new Error("CPF invalid");
          } else {
-            //...
+            employees.findByIdAndUpdate(id, {$set: req.body}, (error) => {
+               if(error){
+                  res.status(500).send({message: `Fail to update - ${error}`})
+               } else {
+                  res.status(201).send({message: `Update done successfully`})
+               }
+            })
          }
       } catch (e) {
          res.status(400).json({ "message error": e.message });
