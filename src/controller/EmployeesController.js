2866673d566450c36b0e8fc5bc8bf3f61b7ca407
Arcenio Souza <arcenio_neto@icloud.com>
2022-04-08 15:45:48 -0300

    feat(employeescontroller.js): :sparkles:Add method saveEmployee

--- src/controller/EmployeesController.js
+++ src/controller/EmployeesController.js
@@ -1,96 +1,89 @@
-import * as validations from '../services/validations.js'
+import * as validations from "../services/validations.js";
+import employees from "../model/employee.js";
 
-class EmployeesController{
-
-   //Método Create --------------------
-   createTable(req, res){
-      //...
-   }
-
+class EmployeesController {
    //Método Create --------------------
    async saveEmployee(req, res) {
-      try{
-         const dataEmployee = await new Promise((resolve, reject) => {
-            resolve([
-               req.body.name,
-               req.body.office,
-               parseFloat(req.body.wage),
-               parseInt(req.body.cpf)
-            ]);
-   
-            reject({"message error": "Problems Accessing Employee Information"})         
-         })
-
+      try {
+         let employee = new employees(req.body);
          //validações
-         const validationsName = validations.validationsName(dataEmployee[0])
-         const validationsOffice = validations.validationsOffice(dataEmployee[1])
-         const validationsCPF = validations.validationsCPF(String(dataEmployee[3]))
+         const validationsName = validations.validationsName(employee.name);
+         const validationsJob = validations.validationsJob(employee.job);
+         const validationsCPF = validations.validationsCPF(employee.cpf);
 
-         if(!validationsName){
-            throw new Error("Problems validating name")
-         } else if(!validationsOffice){
-            throw new Error("Position non-existent")
-         } else if(!validationsCPF){
-            throw new Error("CPF invalid")
+         if (!validationsName) {
+            throw new Error("Problems validating name");
+         } else if (!validationsJob) {
+            throw new Error("Position non-existent");
+         } else if (!validationsCPF) {
+            throw new Error("CPF invalid");
          } else {
-            //...
-         }         
-
-      } catch(e){
-         res.status(400).json(e.message)
+            employee.save((error) => {
+               if (error) {
+                  res.status(500).send({message: `Fail to save - ${error}`})
+               } else {
+                  res.status(201).json(employee)
+               }
+            });
+         }
+      } catch (e) {
+         res.status(400).json(e.message);
       }
    }
 
    //Método Read ----------------------
-   getEmployees(req, res){
+   getEmployees(req, res) {
       //...
    }
 
    //Método Read --------------------
-   getEmployeeId(req, res){
-      const id = req.params.id
+   getEmployeeId(req, res) {
+      const id = req.params.id;
       //...
    }
 
    //Método Delete --------------------
-   deleteEmployee(req, res){
-      const id = req.params.id
+   deleteEmployee(req, res) {
+      const id = req.params.id;
       //...
    }
 
    //Método Update --------------------
-   async updateEmployee(req, res){
-      try{
-         const Employees = await new Promise((resolve, reject) => {         
+   async updateEmployee(req, res) {
+      try {
+         const Employees = await new Promise((resolve, reject) => {
             resolve([
                req.params.id,
                req.body.name,
                req.body.office,
                parseFloat(req.body.wage),
-               parseInt(req.body.cpf)
-            ])
+               parseInt(req.body.cpf),
+            ]);
 
-            reject({"message error": "Problems Accessing Employee Information"})   
-         })
+            reject({
+               "message error": "Problems Accessing Employee Information",
+            });
+         });
 
          //validações
-         const validationsName = validations.validationsName(Employees[1])
-         const validationsOffice = validations.validationsOffice(Employees[2])
-         const validationsCPF = validations.validationsCPF(String(Employees[4]))
+         const validationsName = validations.validationsName(Employees[1]);
+         const validationsOffice = validations.validationsOffice(Employees[2]);
+         const validationsCPF = validations.validationsCPF(
+            String(Employees[4])
+         );
 
-         if(!validationsName){
-            throw new Error("Problems validating name")
-         } else if(!validationsOffice){
-            throw new Error("Position non-existent")
-         } else if(!validationsCPF){
-            throw new Error("CPF invalid")
+         if (!validationsName) {
+            throw new Error("Problems validating name");
+         } else if (!validationsOffice) {
+            throw new Error("Position non-existent");
+         } else if (!validationsCPF) {
+            throw new Error("CPF invalid");
          } else {
             //...
-         } 
-         
-      } catch(e){
-         res.status(400).json({"message error": e.message})
-      }      
+         }
+      } catch (e) {
+         res.status(400).json({ "message error": e.message });
+      }
    }
 }
 
