f0d68acef46648798b361ae7e4664ef2b3854abd
Arcenio Souza <arcenio_neto@icloud.com>
2022-04-14 13:41:06 -0300

    fix(app.js): :bug:Adds manual cors configuration

--- src/app.js
+++ src/app.js
@@ -5,8 +5,6 @@ import cors from 'cors';
 
 
 const app = express();
-app.use(cors())
-app.options('*', cors())
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
@@ -16,6 +14,16 @@ app.listen(port, () => console.log(`Servidor funcionando na porta ${port}`))
 
 db.on("error", () => console.log("Connection error"))
 db.once("open", () => console.log("successfully connected to database"))
+
+app.use(cors())
+
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", '*');
+   res.header("Access-Control-Allow-Credentials", true);
+   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+   next();
+ });
 
 
 routes(app)
