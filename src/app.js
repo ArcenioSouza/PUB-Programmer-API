fd280d1e2a4cf4032fe5e428607aad4ff65bf851
Arcenio Souza <arcenio_neto@icloud.com>
2022-04-14 13:43:48 -0300

    fix(app,js): ) :bug:Add configuration from cors library

--- src/app.js
+++ src/app.js
@@ -17,15 +17,6 @@ db.once("open", () => console.log("successfully connected to database"))
 
 app.use(cors())
 
-app.use(function(req, res, next) {
-   res.header("Access-Control-Allow-Origin", '*');
-   res.header("Access-Control-Allow-Credentials", true);
-   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-   next();
- });
-
-
 routes(app)
 
 export default app;
