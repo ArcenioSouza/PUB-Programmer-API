b5ae74eb077340b9f4445e0a5939ddc3e7908642
Arcenio Souza <arcenio_neto@icloud.com>
2022-04-14 12:01:45 -0300

    fix(app.js): :bug:Adds use of cors to correct errors in requests

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import db from './config/dbConnect.js';
 import routes from './routes/index.js';
+import cors from 'cors';
+
 
 const app = express();
+app.use(cors())
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
